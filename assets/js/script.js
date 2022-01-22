6d45041773559f9d90ce14b7e51e5480c9f7eace
Jose Lemuz <lemuz.jose@gmail.com>
2022-01-21 23:30:21 -0800

    added task with name and type

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,18 @@ var createTaskHandler = function(event) {
   event.preventDefault();
 
   var taskNameInput = document.querySelector("input[name='task-name']").value;
-  var selectNameInput = document.querySelector("select[name='select-dropdown']").value;
-  listItemEl.textContent = taskNameInput;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+  listItemEl.appendChild(taskInfoEl);
+  tasksToDoEl.appendChild(listItemEl);
 };
 
 formEl.addEventListener("submit", createTaskHandler);
